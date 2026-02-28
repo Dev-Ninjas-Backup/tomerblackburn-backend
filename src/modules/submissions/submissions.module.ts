989909a3d9ccc727bdfbe7667ab9b98bd06888d6
MIRZA SAIKAT AHMMED <mirzasaikatahmmed@gmail.com>
2026-03-01 05:31:35 +0600

    ✨ feat: Add zip lookup service to submissions module

--- src/modules/submissions/submissions.module.ts
+++ src/modules/submissions/submissions.module.ts
@@ -4,11 +4,12 @@ import { SubmissionsController } from './submissions.controller';
 import { PrismaModule } from '@/common/prisma/prisma.module';
 import { PdfGeneratorService } from '../pdf/pdf-generator.service';
 import { NotificationsModule } from '../notifications/notifications.module';
+import { ZipLookupService } from './zip-lookup.service';
 
 @Module({
   imports: [PrismaModule, NotificationsModule],
   controllers: [SubmissionsController],
-  providers: [SubmissionsService, PdfGeneratorService],
+  providers: [SubmissionsService, PdfGeneratorService, ZipLookupService],
   exports: [SubmissionsService],
 })
 export class SubmissionsModule {}
